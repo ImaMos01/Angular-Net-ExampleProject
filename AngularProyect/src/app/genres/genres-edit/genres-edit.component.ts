deb302d398defe953983cef595b96af11049c711
ImaMos01 <imoscosoa@unsa.edu.pe>
2025-09-08 21:32:05 -0700

    first form
    Functions to understand how angular edit and add new items in forms

--- AngularProyect/src/app/genres/genres-edit/genres-edit.component.ts
+++ AngularProyect/src/app/genres/genres-edit/genres-edit.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { GenresFormsComponent } from "../genres-forms/genres-forms.component";
+import { genresCreationDTO } from '../genres';
 
 @Component({
   selector: 'app-genres-edit',
-  imports: [],
+  imports: [GenresFormsComponent],
   templateUrl: './genres-edit.component.html',
   styleUrl: './genres-edit.component.css'
 })
 export class GenresEditComponent {
   @Input({transform:numberAttribute})
   id!:number;
+
+genre = {id:1,name:"sci"}
+
+  saveChanges(genre:genresCreationDTO){
+
+  }
 }
