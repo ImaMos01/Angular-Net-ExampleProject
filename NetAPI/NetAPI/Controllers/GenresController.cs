0d60e3ff8e84b4d6bfc19ec023eaee38a3fd0d45
ImaMos01 <imoscosoa@unsa.edu.pe>
2025-10-09 15:46:32 -0700

    Controller and routing

---
+++ NetAPI/NetAPI/Controllers/GenresController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetAPI.Entities;
+using System.Collections.Generic;
+
+namespace NetAPI.Controllers
+{
+    [Route("api/genres")]
+    public class GenresController
+    {
+        [HttpGet]
+        public List<Genre> Get()
+        {
+            List<Genre> repository = new();
+            return repository;
+        }
+    }
+}
